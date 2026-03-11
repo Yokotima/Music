91787df490b77dfbbd1a4fc368a81c1873ca9709
Ascouscous <hugoguyennet1107@gmail.com>
2026-03-11 14:00:23 +0100

    cargo cleaned

--- harmony/src/main.rs
+++ harmony/src/main.rs
@@ -1,3 +1,18 @@
+mod files {
+    pub mod save_to_json;
+}
+use files::save_to_json::{Project, save_to_json,Track,Step};
+
 fn main() {
-    println!("Hello, world!");
+    let project = Project {
+        name: "Harmony".into(),
+        version: "0.1.0".into(),
+        description: "A Rust project management tool".into(),
+        tracks: vec![], 
+    };
+
+    match save_to_json(&project, "../test.json") {
+        Ok(_) => println!("Project saved to test.json"),
+        Err(e) => eprintln!("Error saving project: {}", e),
+    }
 }
