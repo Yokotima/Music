e052dc76006440fa07407b2e88ed214ae6f1d219
Ascouscous <hugoguyennet1107@gmail.com>
2026-03-12 10:29:11 +0100

    minor corrections + load_from_json + export_to_wav + added hound in .toml

--- harmony/src/main.rs
+++ harmony/src/main.rs
@@ -1,18 +1,26 @@
 mod files {
     pub mod save_to_json;
 }
-use files::save_to_json::{Project, save_to_json,Track,Step};
+use files::save_to_json::*;
 
 fn main() {
-    let project = Project {
+    let project = Project 
+    {
         name: "Harmony".into(),
         version: "0.1.0".into(),
         description: "A Rust project management tool".into(),
         tracks: vec![], 
     };
 
-    match save_to_json(&project, "../test.json") {
+    match save_to_json(&project, "../test.json") 
+    {
         Ok(_) => println!("Project saved to test.json"),
         Err(e) => eprintln!("Error saving project: {}", e),
     }
+    match load_from_json("../test.json") 
+    {
+        Ok(project) => println!("Project loaded from test.json"),
+        Err(e) => eprintln!("Error loading project: {}", e),
+    }
+
 }
