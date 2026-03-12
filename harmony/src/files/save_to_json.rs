c0b71649c62cb1ee5cc7aec61735aea96549b4cb
Ascouscous <hugoguyennet1107@gmail.com>
2026-03-12 10:26:07 +0100

    minor corrections + load_from_json + export_to_wav

--- harmony/src/files/save_to_json.rs
+++ harmony/src/files/save_to_json.rs
@@ -3,6 +3,8 @@ extern crate serde_json;
 
 use serde::{Deserialize, Serialize};
 use std::fs;
+use hound;
+use std::f32::consts::PI;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Project {
@@ -37,8 +39,72 @@ pub struct Step {
     pub velocity: Option<f32>,
 }
 
-pub fn save_to_json(project: &Project, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn save_to_json(project: &Project, path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    //le path doit se terminer par .json
+    if !path.ends_with(".json") 
+    {
+        return Err("Path must end with .json (case sensitive)".into());
+    }
     let data = serde_json::to_string_pretty(project)?;
     fs::write(path, data)?;
-    Ok(())
+    Ok(path.into())
+}
+
+pub fn load_from_json(path: &str) -> Result<Project, Box<dyn std::error::Error>> {
+    if !path.ends_with(".json") 
+    {
+        return Err("Path must end with .json (case sensitive)".into());
+    }
+    let data = fs::read_to_string(path)?;
+    let project: Project = serde_json::from_str(&data)?;
+    Ok(project)
+}
+
+pub fn export_to_wav(project: &Project, path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    //pour tt ce qui est formules et tt pour cette fonction est a verifier 
+    //mais si ca marche c good alors
+    
+    if !path.ends_with(".wav") 
+    {
+        return Err("Path must end with .wav (case sensitive)".into());
+    }
+    let sample_rate = 44100;      //44100 => valeur par defaut jsp trop quoi  
+    let spec = hound::WavSpec //mettre dcp jai juste mis ce quon ma conseille
+    {
+        channels: 1,
+        sample_rate,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create(path, spec)?;
+
+    let step_duration = 0.5; 
+
+    for track in &project.tracks
+    {
+        for step in &track.steps 
+        {
+            if !step.active 
+            { 
+                continue; 
+            }
+
+            if let Some(note) = step.note 
+            {
+                let freq = 440.0 * 2f32.powf((note as f32 - 69.0) / 12.0);
+                let velocity = step.velocity.unwrap_or(track.default_velocity);
+
+                let num_samples = (step_duration * sample_rate as f32) as usize;
+                for i in 0..num_samples 
+                {
+                    let t = i as f32 / sample_rate as f32;
+                    let sample = (velocity * i16::MAX as f32 * (2.0 * PI * freq * t).sin()) as i16;
+                    writer.write_sample(sample)?;
+                }
+            }
+        }
+    }
+
+    writer.finalize()?;
+    Ok(path.into())
 }
